7fea946d1875e07572ae5a1d6a6d3be0811ef6ac
hellofrank <hello_wt@126.com>
2013-07-23 15:35:40 +0800

    执行annotate之后，增加相应的数据库注释

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,14 +2,24 @@
 #
 # Table name: companies
 #
-#  id         :integer          not null, primary key
-#  owner      :string(255)
-#  name       :string(255)
-#  about      :string(255)
-#  address    :string(255)
-#  contact    :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :integer          not null, primary key
+#  owner         :string(255)
+#  name          :string(255)
+#  about         :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#  logo          :string(255)
+#  province      :string(255)
+#  city          :string(255)
+#  district      :string(255)
+#  email         :string(255)
+#  phone         :string(255)
+#  telephone     :string(255)
+#  qq            :string(255)
+#  address_other :string(255)
+#  contact_other :string(255)
+#  category      :string(255)
 #
 
 require 'spec_helper'
