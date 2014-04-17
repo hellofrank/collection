6e2b3716357e1d16a8907caa34b29a8f246ce91c
hellofrank <hello_wt@126.com>
2014-04-17 22:08:47 +0800

    just modify

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,14 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.paginate(:page => params[:page], :per_page => 5)
 		@newst_articles = Article.limit(5)
-		@command_articles = Article.where('recommend' => true).limit(5)
+		@recommend_articles = Article.where('recommend' => true).limit(5)
 		@categories = Category.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
 		@newst_articles = Article.limit(7)
-		@command_articles = Article.where('recommend' => true).limit(5)
+		@recommend_articles = Article.where('recommend' => true).limit(5)
 		@keywords = @article.keywords
 		@categories = Category.all
 		#@command_artilces = Article.limit(5)
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
 		@category = Category.find(params[:id])
 		@newst_articles = Article.limit(7)
 		@categories = Category.all
-		@command_artilces = Article.limit(7)
+		@recommend_articles = Article.where('recommend' => true).limit(5)
 		@category_articles = @category.articles.paginate(:page => params[:page], :per_page => 5) 
 	end
 end
